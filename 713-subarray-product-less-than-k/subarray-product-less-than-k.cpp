73e9de19184a9b093759a67f93feeb711ed3504c
Shashank Tulsikar <114328591+Shashank-09@users.noreply.github.com>
2026-09-13 23:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (7.08%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -4,24 +4,23 @@ public:
         if(k <= 1){
             return 0;
         }
-        int  count = 0;
-        int n = nums.size();
+        int left = 0;
+        int count = 0;
+        int prod = 1;
 
-        for(int i = 0; i<n; i++){
-            int product = 1;
-            for(int j = i; j >= 0; j--){
-                product *= nums[j];
-            
-            if(product < k){
-                count++;
-            }else{
-                break;
+        for(int right = 0; right < nums.size(); right++ ){
+            prod *= nums[right];
+
+            while(prod >= k){
+                prod /= nums[left];
+                left++;
             }
-        }
-    }
-      return count;    
-    }
 
-      
+            count += right - left + 1;
+        }
+
+        return count;
+        
+    }
 
 };
